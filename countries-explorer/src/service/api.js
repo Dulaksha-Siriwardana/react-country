d1e098e2953df82561ab55b981a48f2676277c7a
Dulaksha-Siriwardana <it22366122@my.sliit.lk>
2025-05-04 10:58:16 +0530

    API for get country by region and code

--- countries-explorer/src/service/api.js
+++ countries-explorer/src/service/api.js
@@ -24,3 +24,23 @@ export const getCountriesByName = async (name) => {
     return [];
   }
 };
+// Get countries by region
+export const getCountriesByRegion = async (region) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/region/${region}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching countries in ${region}:`, error);
+    throw error;
+  }
+};
+// Get country details by code
+export const getCountryByCode = async (code) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/alpha/${code}`);
+    return response.data[0];
+  } catch (error) {
+    console.error(`Error fetching country with code ${code}:`, error);
+    throw error;
+  }
+};
