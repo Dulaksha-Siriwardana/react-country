e0010903a891e8a580fd90688cb7c188e371b436
Dulaksha-Siriwardana <it22366122@my.sliit.lk>
2025-05-04 11:12:37 +0530

    installing packages

--- countries-explorer/src/App.js
+++ countries-explorer/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CountryPage from "./pages/CountryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <main className="py-8">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/country/:code" element={<CountryPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
